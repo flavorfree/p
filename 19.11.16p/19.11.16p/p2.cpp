9014c95a2fc7b7959db69948702a9b9bd5c0fbc7
flavorfree <1669318747@qq.com>
2019-11-28 19:28:00 +0800

    井字棋

---
+++ 19.11.16p/19.11.16p/p2.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Board{
+public:
+	bool checkwon(vector<vector<int> > board){
+		int row = board.size();
+		//检查每一行的和是是否等于row
+		int i, j, sum;
+		for (i = 0; i < row; i++)
+		{
+			sum = 0;
+			for (j = 0; j < row; j++)
+			{
+				sum += board[i][j];
+			}
+			if (sum == row)
+				return true;
+		}
+		//检查每一列的和是是否等于row
+		for (i = 0; i < row; i++)
+		{
+			sum = 0;
+			for (j = 0; j < row; j++)
+			{
+				sum += board[i][j];
+			}
+			if (sum == row)
+				return true;
+		}
+		//检查主对角线的和是是否等于row
+		sum = 0;
+		for (i = 0; i < row; i++)
+		{
+			sum += board[i][i];
+		}
+		if (sum == row)
+			return true;
+		//检查副对角线的和是是否等于row
+		sum = 0;
+		for (i = 0; i < row; i++)
+		{
+			sum += board[i][row - i - 1];
+		}
+		if (sum == row)
+			return true;
+		return false;
+	}
+};
