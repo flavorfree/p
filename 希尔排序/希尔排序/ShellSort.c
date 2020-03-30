85701cd66752dae90a5dd5e1a00bc813cd968b07
flavorfree <1669318747@qq.com>
2020-03-30 20:06:56 +0800

    希尔排序

---
+++ 希尔排序/希尔排序/ShellSort.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+void ShellSort(int* a, int n)
+{
+	int gap = n ;
+	//gap>1时为预排序	接近有序
+	//gap==1时为直接插入排序	有序
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;//间隔为gap的预排序，+1是保证最后一次一定是1
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int tmp = a[end + gap];
+			while (end >= 0)
+			{
+				if (a[end] > tmp)
+				{
+					a[end + gap] = a[end]; 
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = tmp;
+		}
+	}
+
+}
+
+int main()
+{
+	int a[] = { 9, 2, 3, 4, 8, 4, 1, 7, 5, 6 };
+	ShellSort(a, sizeof(a) / sizeof(int));
+	for (int i = 0; i < sizeof(a) / sizeof(int); i++)
+	{
+		printf("%d ", a[i]);
+	}
+	system("pause");
+	return 0;
+}
