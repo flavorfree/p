bcb858449d320fbb50906ccad6cc99cc2ea048c9
flavorfree <1669318747@qq.com>
2020-04-22 21:05:15 +0800

    求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。

---
+++ 20.4.22/20.4.22/1p.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+//求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、
+//switch、case等关键字及条件判断语句（A?B:C）。
+
+class Sum
+{
+public:
+	Sum()//构造函数
+	{
+		sum += i;
+		i++;
+	}
+	static int DateSum()
+	{
+		return sum;
+	}
+
+private:
+	static int i;
+	static int sum;
+};
+
+int Sum::i = 1;
+int Sum::sum = 0;
+
+class Solution
+{
+public:
+	int Sum_Solutio(int n)
+	{
+		Sum a[5];
+		return Sum::DateSum();
+	}
+};
+
+int main()
+{
+	Solution st;
+	cout << st.Sum_Solutio(5) << endl;
+	system("pause");
+	return 0;
+}
