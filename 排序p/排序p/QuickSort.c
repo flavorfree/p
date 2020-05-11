fb9dbadbaa7e33d0251efe8ad267175cb3af737a
flavorfree <1669318747@qq.com>
2020-05-11 22:36:57 +0800

    p

--- 排序p/排序p/QuickSort.c
+++ 排序p/排序p/QuickSort.c
@@ -3,19 +3,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int Digpit(int* a, int begin,int end)//ÍÚ¿Ó·¨
+void Swap5(int* p1, int* p2)
+{
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+//int Digpit(int* a, int begin,int end)//ÍÚ¿Ó·¨
+//{
+//	int tmp = a[end];
+//	while (begin < end)
+//	{
+//		while (begin < end && a[begin] <= tmp)
+//			begin++;
+//		a[end] = a[begin];
+//		while (begin<end && a[end] >= tmp)
+//			end--;
+//		a[begin] = a[end];
+//	}
+//	a[begin] = tmp;
+//	return begin;
+//}
+
+int _QuickSort(int* a, int begin, int end)	//hoare·¨
 {
 	int tmp = a[end];
 	while (begin < end)
 	{
 		while (begin < end && a[begin] <= tmp)
 			begin++;
-		a[end] = a[begin];
-		while (begin<end && a[end] >= tmp)
+		while (begin < end && a[end] > tmp)
 			end--;
-		a[begin] = a[end];
+		Swap5(&a[begin], &a[end]);
 	}
-	a[begin] = tmp;
+	Swap5(&a[begin], &a[end]);
 	return begin;
 }
 
@@ -25,7 +47,8 @@ void QuickSort(int* a, int begin, int end)
 	if (begin >= end)
 		return;
 
-	int tmp = Digpit(a, begin, end);
+	//int tmp = Digpit(a, begin, end);
+	int tmp = _QuickSort(a, begin, end);
 
 	QuickSort(a, begin, tmp - 1);
 	QuickSort(a, tmp + 1, end);
