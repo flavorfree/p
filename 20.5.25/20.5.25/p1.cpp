aa21bf1e79700e749575ab1648cf828c28278a88
flavorfree <1669318747@qq.com>
2020-05-26 22:55:08 +0800

    leetcode20，28题

---
+++ 20.5.25/20.5.25/p1.cpp
@@ -0,0 +1,70 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//
+////给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+////
+////有效字符串需满足：
+////
+////左括号必须用相同类型的右括号闭合。
+////左括号必须以正确的顺序闭合。
+////注意空字符串可被认为是有效字符串。
+//
+//
+//class Solution 
+//{
+//public:
+//	bool isValid(string s) 
+//	{
+//		vector<char> v;
+//		int j = 0;
+//		for (int i = 0; i<s.size(); i++)
+//		{
+//			if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+//			{
+//				v.push_back(s[i]);
+//				j++;
+//			}
+//			else
+//			{
+//				if (v.empty())
+//					return false;
+//
+//				if (s[i] == ')')
+//					s[i] = '(';
+//				else if (s[i] == '}')
+//					s[i] = '{';
+//				else if (s[i] == ']')
+//					s[i] = '[';
+//
+//				if (j > 0 && v[j - 1] == s[i])
+//				{
+//					v.pop_back();
+//					j--;
+//				}
+//				else
+//					return false;
+//			}
+//		}
+//		return v.empty();
+//	}
+//};
+//
+//int main()
+//{
+//	Solution a;
+//
+//	string s;
+//	while (1)
+//	{
+//		getline(cin, s);
+//		cout << a.isValid(s) << endl;;
+//	}
+//	
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ 20.5.25/20.5.25/p2.cpp
@@ -0,0 +1,54 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+//实现 strStr() 函数。
+//
+//给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 
+//字符串出现的第一个位置(从0开始)。如果不存在，则返回  - 1。
+
+class Solution 
+{
+public:
+	int strStr(string haystack, string needle)
+	{
+		int i = 0, j = 0;
+		while (haystack[i] != '\0' && needle[j] != '\0')
+		{
+			if (needle[j] == haystack[i])
+			{
+				i++, j++;
+			}
+			else    //i+1,j=0
+			{
+				i = i - j + 1;
+				j = 0;
+			}
+		}
+		if (j == needle.size())
+			return i - j;
+		else
+			return -1;
+	}
+};
+
+int main()
+{
+	Solution su;
+
+	while (1)
+	{
+		string s1;
+		getline(cin, s1);
+		string s2;
+		getline(cin, s2);
+
+		cout << su.strStr(s1, s2) << endl;
+	}
+	
+
+	system("pause");
+	return 0;
+}
